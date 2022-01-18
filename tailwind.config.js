250059a7c0e89bea22064b42bad7b5dff28eeaf8
ictus <ictus@ictusui-MacBookPro.local>
2022-01-18 11:36:17 +0900

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'regal-blue': '#243c5a'
+      }
+    },
+  },
+  plugins: [],
+}
