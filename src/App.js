b0648b1fac99fc226092ddddac4f3e4f37739261
ictus <ictus@ictusui-MacBookPro.local>
2022-01-18 15:05:49 +0900

    fin

--- src/App.js
+++ src/App.js
@@ -2,7 +2,219 @@ import './App.css';
 
 function App() {
     return (
+        <div className="p-20 bg-white max-w-5xl ml-auto mr-auto scale-75">
+            <div className="grid grid-cols-3">
+                <div>
+                    <div className='pb-10'>
+                        <img className='rounded-full w-60 shadow-xl'
+                             src="https://moonshadow95.github.io/portfolio/images/avatar.png"
+                             alt="chuljong"/>
+                    </div>
+                    <div className='pb-12'>
+                        <div className="text-amber-700 text-2xl pb-4">
+                            Details
+                        </div>
 
+                        <div className='py-2'>
+                            <div className='pb-2 font-medium'>
+                                Address
+                            </div>
+                            <div className='text-gray-600'>
+                                <div>
+                                    13 Munseong-ro 38-gil
+                                </div>
+                                <div>
+                                    Gwanak-gu, Seoul
+                                </div>
+                                <div>
+                                    Republic of Korea
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='py-2'>
+                            <div className='pb-2 font-medium'>
+                                Phone
+                            </div>
+                            <div className='text-gray-600'>
+                                +82)010-9164-5793
+                            </div>
+                        </div>
+
+                        <div className='py-2'>
+                            <div className='pb-2 font-medium'>
+                                Email
+                            </div>
+                            <div className='text-gray-600'>
+                                last_enemy@ictus.app
+                            </div>
+                        </div>
+                    </div>
+                    <div className="text-amber-700 text-2xl py-4">
+                        Skills
+                    </div>
+                    <div>
+                        <div className='font-medium py-2'>
+                            <div>HTML</div>
+                            <div>⬤⬤⬤⬤⬤</div>
+                        </div>
+                        <div className='font-medium py-2'>
+                            <div>CSS</div>
+                            <div>⬤⬤⬤⬤⬤</div>
+                        </div>
+                        <div className='font-medium py-2'>
+                            <div>JavaScript</div>
+                            <div>⬤⬤⬤⬤⬤</div>
+                        </div>
+                        <div className='font-medium py-2'>
+                            <div>React</div>
+                            <div>⬤⬤⬤⬤⬤</div>
+                        </div>
+                        <div className='font-medium py-2'>
+                            <div>TailwindCSS</div>
+                            <div>⬤⬤⬤⬤⬤</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='col-span-2'>
+                    <div>
+                        <div className='text-4xl'>
+                            <span>Lim Chul Jong</span>
+                        </div>
+                        <div className='text-gray-600 text-xl'>
+                            <small>
+                                Frontend Developer
+                            </small>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="text-amber-700 text-2xl py-2">
+                            Profile
+                        </div>
+                        <div className='text-gray-600 py-4'>
+                            <p className='leading-7 text-gray-600'>
+                                열매를 가지에 돋고, 방지하는 그와 스며들어 원대하고, 듣는다. 창공에 싹이 현저하게 가는 인간의 봄날의 열매를 황금시대다. 우리의 위하여 그들은 부패뿐이다.
+                                우리 싶이 행복스럽고 이것은 바이며, 사막이다. 때까지 인생을 든 것이다. 천고에 갑 그와 꽃이 뛰노는 가슴이 있으랴? 이성은 주며, 뜨고, 것은 어디 사는가
+                                꽃이 힘있다. 미인을 못할 그러므로 황금시대다. 위하여 인류의 소금이라 너의 소리다.이것은 트고, 우리 운다. 같이, 얼마나 있으며, 꽃이 인생의 있다. 불러
+                                품에 무한한 낙원을 간에 능히 이것이다.
+                                위하여서, 아니더면, 것은 끓는 그와 풍부하게 눈에 아니한 이것이다. 새 이상의 찾아 구하지 싶이 구할 거친 것이다. 같지 그것을 피고, 끓는다.
+                            </p>
+                        </div>
+                    </div>
+                    <div className='py-4'>
+                        <div className="text-amber-700 text-2xl py-2">
+                            Employment History
+                        </div>
+                        <ul className='list-disc pl-6'>
+                            <li>
+                                <div className='py-2'>
+                                    <span>Frontend Developer, Ictus</span>
+                                </div>
+                                <div className='flex flex-col'>
+                                    <small>
+                                        Nov.11th.2021 - ing
+                                    </small>
+                                    <small>
+                                        Gasan Digital Complex
+                                    </small>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className='py-4'>
+                        <div className="text-amber-700 text-2xl py-2">
+                            Testimonials
+                        </div>
+                        <ul className='list-disc pl-6'>
+                            <li className='py-2 border-b'>
+                                <div className='flex flex-col justify-center'>
+                                    <span className='text-lg'>
+                                        Sookyung, Lee
+                                    </span>
+                                    <small>
+                                        Teacher
+                                    </small>
+                                </div>
+                                <div>
+                                    <strong>
+                                        오래 잊지 못할 훈련생 중 한명
+                                    </strong>
+                                    <p className="leading-7 text-gray-600">
+                                        인위적으로 꾸미지 않은, 편안한 스타일을 가진 감각과 센스가 있는 친구라고 생각해요.
+                                        주어진 수행과제가 어려워도 포기하지 않고 끝내 해결하는 성실함을 가지고 있어서, '노력은 배신하지 않는다' 는 저의 의견에 힘을 실어주는 좋은
+                                        학생이었어요.
+                                        또 한번 숙지한 것은 잊지 않고 응용하는 영특함을 가지고 있었고, 더 많은 것을 배우고 싶어하는 지적 호기심도 높은 학생이었어요.
+                                        자신의 생각과 소신이 뚜렷하지만, 고집 피우지 않고 다른 의견을 수용할 줄 알아서 오늘보다 내일이 더 기대되는 학생이었지요.
+                                        훈련교사를 하면서 수없이 많은 훈련생을 만나지만, 오랜 시간이 지나도 잊지 못할 훈련생 중에 하나라고 확신해요.
+                                    </p>
+                                </div>
+                            </li>
+                            <li className='py-2 border-b'>
+                                <div className='flex flex-col justify-center'>
+                                    <span className='text-lg'>
+                                        Eunyoung, Lee
+                                    </span>
+                                    <small>
+                                        Student
+                                    </small>
+                                </div>
+                                <div>
+                                    <strong>
+                                        학생들 사이 또 한명의 선생님
+                                    </strong>
+                                    <p className="leading-7 text-gray-600">
+                                        철종이는 어려운 수업에서도 최대한 자신의 답을 내려고 하는 모습을 보였다.
+                                        풀리지 않는 부분에서는 선생님과 무엇이 틀렸고 달랐는지 인지하고 다시 문제를 풀어보는 모습을 본받고싶었다.
+                                        학원생들중에서 제일 어린데도 불구하고 다른 학원생들의 어려움을 무시하지 않고 자신이 아는 지식을 최대한 공유해주며, 때론 선생님처럼 그 사람의
+                                        눈높이에
+                                        맞게 설명해주는 능력도 갖추고 있었다.
+                                        그래서 학원을 다니면서 철종이에게 제일 고마웠다.
+                                    </p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className='py-4'>
+                        <div className="text-amber-700 text-2xl py-2">
+                            Education
+                        </div>
+                        <ul className='list-disc pl-6'>
+                            <li className='py-2 border-b my-2'>
+                                <div className='flex items-center'>
+                                    <span className='pr-4'>
+                                        Gukje Computer Academy
+                                    </span>
+                                    <small>
+                                        Mar.2020 - Oct.2020
+                                    </small>
+                                </div>
+                            </li>
+                            <li className='py-2 border-b my-2'>
+                                <div className='flex items-center'>
+                                    <span className='pr-4'>
+                                        Nomad Coders Academy
+                                    </span>
+                                    <small>
+                                        Oct.2020 - ing
+                                    </small>
+                                </div>
+                            </li>
+                            <li className='py-2 border-b my-2'>
+                                <div className='flex items-center'>
+                                    <span className='pr-4'>
+                                        Dream Coding Academy
+                                    </span>
+                                    <small>
+                                        Nov.2020 - ing
+                                    </small>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
